b2f7aa90a317d29acf3259ac00c1910f62a56385
SqeeZarion <yrapetruha0903@gmail.com>
2023-01-10 03:05:31 +0200

    Enity, with which I work

---
+++ JarPControlProject/Enity/PCControl.cs
@@ -0,0 +1,23 @@
+﻿namespace JarPControlProject.Enity;
+
+class PCControl
+{
+    // A field containing the name of the computer
+    private String computerName;
+
+    // A field containing a list of open desktop programs
+    private List<String> openPrograms;
+
+    // A field indicating whether music is playing
+    private Boolean isMusicPlaying;
+
+    // A field containing the current sound volume
+    private int volume;
+
+    public PCControl(String computerName) {
+        this.computerName = computerName;
+        this.openPrograms = new List<string>();
+        this.isMusicPlaying = false;
+        this.volume = 50;
+    }
+}
---
+++ JarPControlProject/Enity/TelegramBot.cs
@@ -0,0 +1,15 @@
+﻿namespace JarPControlProject.Enity;
+
+public class TelegramBot
+{
+    // A field containing the bot's token to access the Telegram API
+    private String token;
+
+    // Field containing the ID of the chat from which the commands will be received
+    private long chatId;
+
+    public TelegramBot(String token, long chatId) {
+        this.token = token;
+        this.chatId = chatId;
+    }
+}
