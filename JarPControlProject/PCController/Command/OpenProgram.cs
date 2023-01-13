256b472ac6f1839cc3ea902e0133926854d95e1e
SqeeZarion <yrapetruha0903@gmail.com>
2023-01-13 04:11:22 +0200

    Function

---
+++ JarPControlProject/PCController/Command/OpenProgram.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using JarPControlProj.Enity;
+
+namespace JarPControlProject.PCController.Command;
+
+public class OpenProgram : CommandInterface
+{
+    private PCControl pcControl;
+
+    public OpenProgram(PCControl pcControl)
+    {
+        this.pcControl = pcControl;
+    }
+
+    public CommandResult<String> Execute(String programName)
+    {
+        // Check if the program is already open
+        if (!pcControl.OpenProgram.Contains(programName))
+        {
+            try
+            {
+                // Open the program
+                Process.Start(programName);
+                pcControl.OpenProgram.Add(programName);
+                Console.WriteLine("Program " + programName + " opened successfully.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to open program. Error: " + e.Message);
+            }
+        }
+        else
+            Console.WriteLine("Program " + programName + " is already open.");
+
+        CommandResult<String> result = new CommandResult<String>("Succeed!!!!!", "Deleting works!", true);
+        return result;
+    }
+}
