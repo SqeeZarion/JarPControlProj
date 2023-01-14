3d8ea1665a404ac139026e4c8df610a661ef7a5d
SqeeZarion <yrapetruha0903@gmail.com>
2023-01-14 04:12:34 +0200

    Function

---
+++ JarPControlProject/PCController/Command/Basis/IncreaseVolume.cs
@@ -0,0 +1,30 @@
+﻿using JarPControlProj.Enity;
+
+namespace JarPControlProject.PCController.Command;
+
+public class IncreaseVolume : CommandInterface
+{
+    private PCControl pcControl;
+    private CommandResult<String> result;
+
+    public IncreaseVolume(PCControl pcControl)
+    {
+        this.pcControl = pcControl;
+    }
+
+    public CommandResult<String> Execute(String amountSTR)
+    {
+        int amount = int.Parse(amountSTR);
+
+        if (pcControl.Volume + amount <= 100)
+        {
+            pcControl.Volume += amount;
+
+            result = new CommandResult<String>("Succeed!", "Volume increased to: " + pcControl.Volume, true);
+        }
+        else
+            result = new CommandResult<String>("Failed!", "Cannot increase volume above 100.", false);
+
+        return result;
+    }
+}
