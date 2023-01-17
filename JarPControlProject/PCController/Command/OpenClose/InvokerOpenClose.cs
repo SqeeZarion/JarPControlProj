8e18b23c627c38586320aa3116316b6b6aad59c7
SqeeZarion <yrapetruha0903@gmail.com>
2023-01-17 02:27:06 +0200

    Invoker

---
+++ JarPControlProject/PCController/Command/OpenClose/InvokerOpenClose.cs
@@ -0,0 +1,26 @@
+﻿namespace JarPControlProject.PCController.Command;
+
+public class InvokerOpenClose
+{
+    private ComInterfaceOpenClose command;
+
+    public InvokerOpenClose(ComInterfaceOpenClose command)
+    {
+        this.command = command;
+    }
+
+    public void doSmth()
+    {
+        try
+        {
+            command.Execute(@"D:\Program Files (x86)\Telegram Desktop\Telegram.exe");
+        }
+       
+        catch (IOException e)
+        {
+            if (e.Source != null)
+                Console.WriteLine("IOException source: {0}", e.Source);
+            throw;
+        }
+    }
+}
