13d22a51e1d2a2d2eb255cfa6e2a1f37630cc8ad
SqeeZarion <yrapetruha0903@gmail.com>
2023-01-17 02:28:23 +0200

    Music mode(command)

---
+++ JarPControlProject/PCController/Command/Music/MusicCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+public class MusicCommand
+{
+    public const int KEYEVENTF_EXTENTEDKEY = 1;
+    public const int VK_MEDIA_NEXT_TRACK = 0xB0;
+    public const int VK_MEDIA_PLAY_PAUSE = 0xB3;
+    public const int VK_MEDIA_PREV_TRACK = 0xB1;
+    
+    [DllImport("user32.dll")]
+    public static extern void keybd_event(byte virtualKey, byte scanCode, uint flags, IntPtr extraInfo);
+
+    public void PlayPause()
+    {
+        keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+    }
+
+    public void NextTrack()
+    {
+        keybd_event(VK_MEDIA_NEXT_TRACK, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+    }
+
+    public void PreviousTrack()
+    {
+        keybd_event(VK_MEDIA_PREV_TRACK, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+    }
+}
